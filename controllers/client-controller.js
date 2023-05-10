bdeed6f7dd78fd3bfd4d4bd68e2134221ecef685
Evgeniy <zhenya.todorov@gmail.com>
2023-05-10 11:02:48 +0300

    fix

--- controllers/client-controller.js
+++ controllers/client-controller.js
@@ -139,7 +139,7 @@ class ClientController {
       if (!tokenData) {
         throw ApiError.BadRequerest(req.t('CONTROLLER.CLIENT.SEND_CODE.NOT_USER'));
       }
-      const UserData = await clientService.findById(id);
+      const UserData = await clientService.findById(tokenData.id);
       if (!UserData) {
         throw ApiError.BadRequerest(req.t('CONTROLLER.CLIENT.SEND_CODE.NOT_USER'));
       }
